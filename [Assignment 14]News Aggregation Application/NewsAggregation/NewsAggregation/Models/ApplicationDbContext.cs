a2cd08c475e91f65fbe9e348384fba42762ab86a
meumocdev <113320440+meumocdev@users.noreply.github.com>
2024-09-30 21:41:42 +0700

    update project
    Update project

---
+++ [Assignment 14]News Aggregation Application/NewsAggregation/NewsAggregation/Models/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NewsAggregation.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<News> News { get; set; }
+        public DbSet<RSS> RSSFeeds { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<View> Views { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
